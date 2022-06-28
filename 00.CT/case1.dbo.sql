99573dca8cd0ef64c090fd48f9455842b6a6b6d1
Emi260 <65983384+Emi260@users.noreply.github.com>
2022-06-28 15:23:41 -0300

    2

--- 00.CT/case1.dbo.sql
+++ 00.CT/case1.dbo.sql
@@ -9,3 +9,13 @@ CREATE TABLE DIP.personas(
 );
 
 --rollback DROP TABLE DIP.personas;
+
+--changeset case:2
+CREATE TABLE DIP.personajes(
+    person_id NUMBER,
+    first_name VARCHAR2(50) NOT NULL,
+    last_name VARCHAR2(50) NOT NULL,
+    PRIMARY KEY(person_id)
+);
+
+--rollback DROP TABLE DIP.personajes;
